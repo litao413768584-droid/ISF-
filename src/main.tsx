8aad4c98d6cbd02c95b7f7ebd5bc382e31d1e8fa
石油计量 <litao413768584@gmail.com>
2026-08-06 19:25:53 +0800

    版本兼容问题

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+﻿import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 捕获错误
+const root = document.getElementById('root');
+if (!root) {
+  console.error('❌ 找不到 root 元素');
+} else {
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
